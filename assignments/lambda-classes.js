0e58e347274c2b26f2d1d3423406dd1ae962a884
Matt Hagner <web@matthagner.com>
2019-04-25 19:09:40 -0500

    Complete Student class implementation

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -48,3 +48,44 @@ class Instructor extends Person {
     console.log(`${student.name} receives a perfect score on ${subject}`)
   }
 }
+
+/*
+ * Student < Person
+ *
+ * previousBackground: string
+ * className: string
+ * favSubjects: string[]
+ *
+ * listSubjects: () => void
+ * lists all of the students favorite subjects, one by one.
+ *
+ * PRAssignment: string => void
+ * Receives the subject as a string and logs out 
+ * `student.name has submitted a PR for {subject}`
+ *
+ * sprintChallenge: string => void
+ * Receives subject as a string and logs out
+ * `student.name has begun sprint challenge on {subject}`
+ *
+ * */
+
+class Student extends Person {
+  constructor(attrs) {
+    super(attrs);
+    this.previousBackground = attrs.previousBackground;
+    this.className = attrs.className;
+    this.favSubjects = attrs.favSubjects;
+  }
+
+  listSubjects() {
+    this.favSubjects.forEach(console.log);
+  }
+
+  PRAssignment(subject) {
+    console.log(`${this.name} has submitted a PR for ${subject}`);
+  }
+
+  sprintChallenge(subject) {
+    console.log(`${this.name} has begun sprint challenge on ${subject}`);
+  }
+}
